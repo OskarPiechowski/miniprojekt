90867d06a3239296064610d8646a10e5925b39ef
Oskar <o.piechowski@gmail.com>
2023-03-12 15:43:09 +0100

    Initial commit

---
+++ src/test/java/com/miniprojekt/miniprojekt/MiniprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniprojekt.miniprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniprojektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
